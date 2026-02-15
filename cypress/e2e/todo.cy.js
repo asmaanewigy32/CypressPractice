646eb6042c6d8e1739bf6567e7e2bb64ce1c6365
Asmaa <asmaanewigy132@gmail.com>
2026-02-15 14:40:34 +0200

    Edited user login test case and added test case for adding a todo

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,10 +1,15 @@
   /// <referance types= "cypress" />
 
-  it("Should be able to login using valid mail and password",() => {
+  it("Should be able to add a todo",() => {
+
     cy.visit("/");
     cy.get('[data-testid="email"]').type("test23@example.com");
     cy.get('[data-testid="password"]').type("test-12345");
     cy.get('[data-testid="submit"]').click();
     cy.get('[data-testid="welcome"]').should("be.visible");
+    cy.get('[data-testid="add"]').click();
+    cy.get('[data-testid="new-todo"]').type("Learn Cypress");
+    cy.get('[data-testid="submit-newTask"]').click();
+    cy.get('[data-testid="todo-item"]').eq(0).should('have.text',"Learn Cypress");
 
   });
