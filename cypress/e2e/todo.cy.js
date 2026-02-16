f9f149054f944bbecf385e616e7a4621bb59c3d9
Asmaa <asmaanewigy132@gmail.com>
2026-02-16 13:57:23 +0200

    Used beforeEach method to send requests and reduce code duplication

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,56 +1,59 @@
+/// <referance types="cypress" />
 import { faker } from '@faker-js/faker';
-/// <referance types= "cypress" />
 
-it("Should be able to add a todo", () => {
+describe("todo test cases", () => {
 
-    cy.request({
-        url: "/api/v1/users/register",
-        method: "POST",
-        body: {
-            firstName: faker.person.firstName(),
-            lastName: faker.person.lastName(),
-            email: faker.internet.email(),
-            password: "Test-12345",
-        },
+    let token;
+    beforeEach(() => {
+        cy.request({
+            url: "/api/v1/users/register",
+            method: "POST",
+            body: {
+                firstName: faker.person.firstName(),
+                lastName: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: "Test-12345",
+            },
+        }).then(response => {
+            token = response.body.access_token
+
+        })
+
+    })
+
+    it("Should be able to add a todo", () => {
+
+        cy.visit("/");
+        cy.get('[data-testid="add"]').click();
+        cy.get('[data-testid="new-todo"]').type("Learn Cypress");
+        cy.get('[data-testid="submit-newTask"]').click();
+        cy.get('[data-testid="todo-item"]').eq(0).should('have.text', "Learn Cypress");
+
     });
 
-    cy.visit("/");
-    cy.get('[data-testid="add"]').click();
-    cy.get('[data-testid="new-todo"]').type("Learn Cypress");
-    cy.get('[data-testid="submit-newTask"]').click();
-    cy.get('[data-testid="todo-item"]').eq(0).should('have.text', "Learn Cypress");
+    it("Should be able to mark a todo as completed", () => {
 
-});
-
-it("Should be able to mark a todo as completed", () => {
-
-    cy.request({
-        url: "/api/v1/users/register",
-        method: "POST",
-       body: {
-            firstName: faker.person.firstName(),
-            lastName: faker.person.lastName(),
-            email: faker.internet.email(),
-            password: "Test-12345",
-        },
-    }).then((response) => {
         cy.request({
             url: "/api/v1/tasks",
             method: "POST",
             auth: {
-                bearer: response.body.access_token
+                bearer: token,
             },
             body: {
                 "item": "learn Cypress",
                 "isCompleted": false
             },
-        })
+
+        });
+
+        cy.visit("/");
+        cy.get('[data-testid="complete-task"]').eq(0).click();
+        cy.get('[data-testid="todo-item"]')
+            .eq(0)
+            .should('have.css', 'background-color', 'rgb(33, 76, 97)');
+
     });
+})
 
-    cy.visit("/");
-    cy.get('[data-testid="complete-task"]').eq(0).click();
-    cy.get('[data-testid="todo-item"]')
-        .eq(0)
-        .should('have.css', 'background-color', 'rgb(33, 76, 97)');
 
-});
+
