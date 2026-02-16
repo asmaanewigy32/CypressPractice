3e670b59d476ff9de0e4a0bcbeffbdedbbbbff29
Asmaa <asmaanewigy132@gmail.com>
2026-02-16 13:32:00 +0200

    Used signup api to avid user status

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,13 +1,15 @@
-  /// <referance types= "cypress" />
+/// <referance types= "cypress" />
 
-  it("Should be able to add a todo",() => {
+it("Should be able to add a todo", () => {
 
     cy.request({
-        url : "/api/v1/users/login",
+        url: "/api/v1/users/register",
         method: "POST",
-        body:{
-            "email":"test23@example.com",
-            "password":"test-12345",
+        body: {
+            firstName: "asmaa",
+            lastName: "latif",
+            email: "asmaa5@exmaple.com",
+            password: "Test-12345",
         },
     });
 
@@ -15,29 +17,32 @@
     cy.get('[data-testid="add"]').click();
     cy.get('[data-testid="new-todo"]').type("Learn Cypress");
     cy.get('[data-testid="submit-newTask"]').click();
-    cy.get('[data-testid="todo-item"]').eq(0).should('have.text',"Learn Cypress");
+    cy.get('[data-testid="todo-item"]').eq(0).should('have.text', "Learn Cypress");
 
-  });
+});
 
-  it("Should be able to mark a todo as completed",() => {
+it("Should be able to mark a todo as completed", () => {
 
-     cy.request({
-        url : "/api/v1/users/login",
+    cy.request({
+        url: "/api/v1/users/register",
         method: "POST",
-        body:{
-            "email":"test23@example.com",
-            "password":"test-12345",
+        body: {
+           firstName: "asmaa",
+            lastName: "latif",
+            email: "asmaa3@exmaple.com",
+            password: "Test-12345",
         },
-    }).then((response) =>{
+    }).then((response) => {
         cy.request({
-            url : "/api/v1/tasks",
-            method : "POST",
-            auth : {
-                bearer : response.body.access_token
+            url: "/api/v1/tasks",
+            method: "POST",
+            auth: {
+                bearer: response.body.access_token
             },
-            body : {
-                "item":"learn Cypress",
-                "isCompleted":false},
+            body: {
+                "item": "learn Cypress",
+                "isCompleted": false
+            },
         })
     });
 
@@ -45,6 +50,6 @@
     cy.get('[data-testid="complete-task"]').eq(0).click();
     cy.get('[data-testid="todo-item"]')
         .eq(0)
-        .should('have.css','background-color','rgb(33, 76, 97)');
+        .should('have.css', 'background-color', 'rgb(33, 76, 97)');
 
-  });
+});
